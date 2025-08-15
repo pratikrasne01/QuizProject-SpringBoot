433fb1179327c4f8a2205b814298e92002faf464
Pratik <pratik.rasne96@gmail.com>
2025-08-16 00:26:09 +0530

    Updated QuestionController and QuestionDao

--- quizapp/src/main/java/com/example/quizapp/controller/QuestionController.java
+++ quizapp/src/main/java/com/example/quizapp/controller/QuestionController.java
@@ -3,7 +3,6 @@ package com.example.quizapp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityReturnValueHandler;
-
 import com.example.quizapp.model.Question;
 import com.example.quizapp.service.QuestionService;
 
